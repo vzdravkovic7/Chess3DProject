ebf7e819a00e4b31ebca987aef61b4d68440ee3a
vzdravkovic7 <141317094+vzdravkovic7@users.noreply.github.com>
2024-09-13 15:11:02 +0200

    [ADD] Grid Placement and README Explanation

---
+++ Assets/Scripts/Chessboard.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+
+public class Chessboard : MonoBehaviour
+{
+    [Header("Art stuff")]
+    [SerializeField] private Material tileMaterial;
+    [SerializeField] private float tileSize = 1.0f;
+    [SerializeField] private float yOffset = 0.2f;
+    [SerializeField] private Vector3 boardCenter = Vector3.zero;
+
+    // LOGIC
+    private const int TILE_COUNT_X = 8;
+    private const int TILE_COUNT_Y = 8;
+    private GameObject[,] tiles;
+    private Camera currentCamera;
+    private Vector2Int currentHover;
+    private Vector3 bounds;
+
+    private void Awake() {
+        GenerateAllTiles(tileSize, TILE_COUNT_X, TILE_COUNT_Y);
+    }
+
+    private void Update() {
+        if (!currentCamera) {
+            currentCamera = Camera.main;
+            return;
+        }
+
+        RaycastHit info;
+        Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out info, 100, LayerMask.GetMask("Tile", "Hover"))) {
+            // Get the indexes of the tile I've hit
+            Vector2Int hitPosition = LookupTileIndex(info.transform.gameObject);
+
+            // If we're hovering a tile after not hovering a tile
+            if(currentHover == -Vector2Int.one) {
+                currentHover = hitPosition;
+                tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+            }
+
+            // If we were already hovering a tile, change the previous one
+            if (currentHover != hitPosition) {
+                tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
+                currentHover = hitPosition;
+                tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+            }
+
+        } else {
+            if (currentHover != -Vector2Int.one) {
+                tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
+                currentHover = -Vector2Int.one;
+            }
+        }
+    }
+
+    // Generate the board
+    private void GenerateAllTiles(float tileSize, int tileCountX, int tileCountY) {
+        yOffset += transform.position.y;
+        bounds = new Vector3((tileCountX / 2) * tileSize, 0, (tileCountX / 2) * tileSize) + boardCenter;
+
+        tiles = new GameObject[tileCountX, tileCountY];
+
+        for (int x = 0; x < tileCountX; x++)
+            for (int y = 0; y < tileCountY; y++)
+                tiles[x, y] = GenerateSingleTile(tileSize, x, y);
+    }
+
+    private GameObject GenerateSingleTile(float tileSize, int x, int y) {
+        GameObject tileObject = new GameObject(string.Format("X:{0}, Y:{1}", x, y));
+        tileObject.transform.parent = transform;
+
+        Mesh mesh = new Mesh();
+        tileObject.AddComponent<MeshFilter>().mesh = mesh;
+        tileObject.AddComponent<MeshRenderer>().material = tileMaterial;
+
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(x * tileSize, yOffset, y * tileSize) - bounds;
+        vertices[1] = new Vector3(x * tileSize, yOffset, (y + 1) * tileSize) - bounds;
+        vertices[2] = new Vector3((x + 1) * tileSize, yOffset, y * tileSize) - bounds;
+        vertices[3] = new Vector3((x + 1) * tileSize, yOffset, (y + 1) * tileSize) - bounds;
+
+        int[] tris = new int[] { 0, 1, 2, 1, 3, 2};
+
+        mesh.vertices = vertices;
+        mesh.triangles = tris;
+
+        mesh.RecalculateNormals();
+
+        tileObject.layer = LayerMask.NameToLayer("Tile");
+        tileObject.AddComponent<BoxCollider>();
+
+        return tileObject;
+    }
+
+    // Operations
+    private Vector2Int LookupTileIndex(GameObject hitInfo) {
+        for (int x = 0; x < TILE_COUNT_X; x++)
+            for (int y = 0; y < TILE_COUNT_Y; y++)
+                if(tiles[x, y] == hitInfo)
+                    return new Vector2Int(x, y);
+
+        return -Vector2Int.one; // Invalid
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Chess3DProject
  
+3D Chess Tutorial Project i wanted to learn with a different approach to creating a game in Unity and different Assets. Source: https://youtu.be/qVhG6ZWqD-o?si=TX3OpkYlf1DWjSzh
