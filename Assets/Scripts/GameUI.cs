8348a6d2ec1eca6b6c2f9fbb0a5ab634b5b9399c
vzdravkovic7 <141317094+vzdravkovic7@users.noreply.github.com>
2024-09-18 11:46:45 +0200

    [ADD] Client and Server Logic

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -1,10 +1,15 @@
+using TMPro;
 using UnityEngine;
 
 public class GameUI : MonoBehaviour
 {
     public static GameUI Instance {  get; set; }
 
+    public Server server;
+    public Client client;
+
     [SerializeField] private Animator menuAnimator;
+    [SerializeField] private TMP_InputField addressInput;
 
     public void Awake() {
         Instance = this;
@@ -13,6 +18,8 @@ public class GameUI : MonoBehaviour
 
     public void OnLocalGameButton() {
         menuAnimator.SetTrigger("InGameMenu");
+        server.Init(8007);
+        client.Init("127.0.0.1", 8007);
     }
 
     public void OnOnlineGameButton() {
@@ -20,11 +27,13 @@ public class GameUI : MonoBehaviour
     }
 
     public void OnOnlineHostButton() {
+        server.Init(8007);
+        client.Init("127.0.0.1", 8007);
         menuAnimator.SetTrigger("HostMenu");
     }
 
     public void OnOnlineConnectButton() {
-        Debug.Log("OnOnlineConnectButton"); // $$
+        client.Init(addressInput.text, 8007);
     }
 
     public void OnOnlineBackButton() {
@@ -32,6 +41,8 @@ public class GameUI : MonoBehaviour
     }
 
     public void OnHostBackButton() {
+        server.Shutdown();
+        client.Shutdown();
         menuAnimator.SetTrigger("OnlineMenu");
     }
 }
---
+++ Assets/Scripts/Net/NetMessage/NetMessage.cs
@@ -0,0 +1,23 @@
+using Unity.Collections;
+using Unity.Networking.Transport;
+
+public class NetMessage
+{
+    public OpCode Code { get; set; }
+
+    public virtual void Serialize(ref DataStreamWriter writer) {
+        writer.WriteByte((byte)Code);
+    }
+
+    public virtual void Deserialize(DataStreamReader reader) {
+
+    }
+
+    public virtual void ReceivedOnClient() {
+
+    }
+
+    public virtual void ReceivedOnServer(NetworkConnection cnn) { 
+    
+    }
+}
